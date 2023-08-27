6be6704d01ff79619bf27c9d84a760424f952a4c
Bomote <bomote.chris@gmail.com>
2023-08-27 14:21:28 +0100

    added referral btn features

--- main.js
+++ main.js
@@ -98,31 +98,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Get button anchors and pyramid elements
     const btnAnchors = document.querySelectorAll(".btn");
-    const referralBtn = document.getElementById("referral-btn");
-    const revealBtn = document.getElementById("reveal-btn")
     const pyramidElements = pyramidSizes.map(size => document.getElementById(size.id)); 
 
     // Click count for button interactions
     let clickCount = 0;
-
-    referralBtn.addEventListener("click",function () {
-
-        const referralContainer = document.getElementById("reveal");
-        const referralNum = document.createElement("div");
-        referralContainer.appendChild(referralNum);
-
-        referralNum.classList.add("referral-num");
-
-        // Wait for 1 second and then update content
-        setTimeout(function() {
-            referralNum.textContent = "12345"; // Placeholder numbers
-            referralBtn.style.display = "none"; // Hide the referral button
-        }, 300);
-    });
-
-    
-
-    
+        
   // Function to add pyramids back with a delay and interval
 const addBackPyramids = () => {
     let index = clickCount - 1; // Index of the last removed pyramid
@@ -135,11 +115,13 @@ const addBackPyramids = () => {
             index--;
         } else {
             clearInterval(addPyramidInterval); // Stop the interval
-            referralBtn.style.display = "none"; // Hide the referral button
-            revealBtn.style.display = "block"; // Show the reveal button
         }
     }, interval);
 };
+
+const referralBtn = document.getElementById('referral-btn');
+const referralText = document.getElementById('reveal-btn');
+
 
 // Add click event listeners to button anchors
 btnAnchors.forEach((anchor, index) => {
@@ -151,12 +133,9 @@ btnAnchors.forEach((anchor, index) => {
             clickCount++;
 
             if (clickCount === btnAnchors.length) {
-                referralBtn.style.display = "block";
-                referralBtn.classList.add("pulse");
-                revealBtn.style.display = "none";
-                            
+                referralText.style.display = "none"
+                referralBtn.style.display = "inline-block"
                 addBackPyramids();
-                
             } else {
                 pulseRemainingPyramids();
             }
@@ -180,50 +159,76 @@ function removePulseFromRemainingPyramids() {
 
     // Function to apply the pulse animation to remaining pyramids
 function pulseRemainingPyramids() {
-    pyramidSizes.forEach((pyramid, index) => {
-        if (index >= clickCount) {
-            const side = document.querySelectorAll(".side" + pyramid.id.substring(7));
-            side[0].classList.add("pulse");
-            side[1].classList.add("pulse");
-            side[2].classList.add("pulse");
-            side[3].classList.add("pulse");
-        }
-    });
-    // Remove the pulse class from remaining pyramids after 2 seconds
-    setTimeout(removePulseFromRemainingPyramids, 2000);
-}
-    
-    const numbersPlaceholder = document.getElementById("numbers-placeholder");
-
-    referralBtn.addEventListener("click", function() {
-        // Apply the pulsing animation class
-        referralBtn.classList.add("pulse");
-
-        // Wait for 1 second and then update content
-        setTimeout(function() {
-            numbersPlaceholder.textContent = "12345"; // Placeholder numbers
-            referralBtn.style.display = "none"; // Hide the referral button
-        }, 1000);
-    });
-
+        pyramidSizes.forEach((pyramid, index) => {
+            if (index >= clickCount) {
+                const side = document.querySelectorAll(".side" + pyramid.id.substring(7));
+                side[0].classList.add("pulse");
+                side[1].classList.add("pulse");
+                side[2].classList.add("pulse");
+                side[3].classList.add("pulse");
+            }
+        });
+        // Remove the pulse class from remaining pyramids after 2 seconds
+        setTimeout(removePulseFromRemainingPyramids, 2000);
+    }    
 });
 
-const toggleNav = () => {
-    const burgerOpen = document.querySelector('.fa-bars');
-    const navbar = document.querySelector('.navbar');
-    const burgerClose = document.querySelector('.fa-xmark')
+var animateButton = function(e) {
 
-    // Toggle navigation links when the hamburger icon is clicked
-    burgerOpen.addEventListener('click', () => {
-        navbar.style.display = "flex";
-        burgerOpen.style.display = "none"
-        burgerClose.style.display = "block"
-    });
-    burgerClose.addEventListener('click', () => {
-        navbar.style.display = "none";
-        burgerOpen.style.display = "block"
-        burgerClose.style.display = "none"
-    });
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  };
+
+
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("referral-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-toggleNav();
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function copyToClipboard(element) {
+    const text = element.textContent;
+  
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        console.log('Text copied to clipboard: ', text);
+      })
+      .catch((error) => {
+        console.error('Error copying text to clipboard: ', error);
+      });
+
+      modal.style.display = "none";
+  }
+  
